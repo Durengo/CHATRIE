5a017c0a6bfd16fea7e190ec8c71e3d8c0cf539f
Sebastian <Sebastian.Termen@gmail.com>
2024-03-02 18:00:54 +0200

    Added necessary Jwt dependencies as well as Lombok for boilerplate code.

--- Server/CHATRIE/build.gradle.kts
+++ Server/CHATRIE/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.12")
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-     implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
 
     // TESTING
     // https://mvnrepository.com/artifact/junit/junit
@@ -44,6 +44,18 @@ dependencies {
     // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
+    // JWT Auth
+    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-api
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-impl
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-jackson
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+    // Lombok
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.test {
