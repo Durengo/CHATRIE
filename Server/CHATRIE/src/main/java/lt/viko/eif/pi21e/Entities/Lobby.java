80b0087d667784de6bb9ed2e549385a05390738b
Sebastian <Sebastian.Termen@gmail.com>
2024-03-02 18:01:32 +0200

    Lombok again...

--- Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Entities/Lobby.java
+++ Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Entities/Lobby.java
@@ -1,47 +1,22 @@
 package lt.viko.eif.pi21e.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.util.UUID;
 
 @Table("lobbies")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Lobby {
     @PrimaryKey
     private UUID lobbyId;
     private String user1Nickname;
     private String user2Nickname;
-
-    public Lobby() {
-    }
-
-    public Lobby(UUID lobbyId, String user1Nickname, String user2Nickname) {
-        this.lobbyId = lobbyId;
-        this.user1Nickname = user1Nickname;
-        this.user2Nickname = user2Nickname;
-    }
-
-    public UUID getLobbyId() {
-        return lobbyId;
-    }
-
-    public void setLobbyId(UUID lobbyId) {
-        this.lobbyId = lobbyId;
-    }
-
-    public String getUser1Nickname() {
-        return user1Nickname;
-    }
-
-    public void setUser1Nickname(String user1Nickname) {
-        this.user1Nickname = user1Nickname;
-    }
-
-    public String getUser2Nickname() {
-        return user2Nickname;
-    }
-
-    public void setUser2Nickname(String user2Nickname) {
-        this.user2Nickname = user2Nickname;
-    }
 }
