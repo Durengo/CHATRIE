21ab1322c53d1181cb5617788dfe1a06df93cca1
Sebastian <Sebastian.Termen@gmail.com>
2024-03-02 18:02:12 +0200

    Authentication process.

---
+++ Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Authentication/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package lt.viko.eif.pi21e.Authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
