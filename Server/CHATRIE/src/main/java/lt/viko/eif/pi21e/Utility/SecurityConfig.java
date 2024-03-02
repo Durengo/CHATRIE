d305b9003bb126f2db5e9a1a68aa8e8b5989fb9b
Sebastian <Sebastian.Termen@gmail.com>
2024-03-02 17:58:43 +0200

    Cleaned up the SecuirtyConfig.

--- Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Utility/SecurityConfig.java
+++ Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Utility/SecurityConfig.java
@@ -1,47 +1,45 @@
 package lt.viko.eif.pi21e.Utility;
 
-import lt.viko.eif.pi21e.Services.UserDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-
-    @Autowired
-    private UserDetailService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/api/v1/auth/demo").authenticated()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/users/**").permitAll()
                         .requestMatchers("/chat/**").authenticated()
-//                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().permitAll()
                 )
-                .formLogin(login -> login.loginPage("/login").permitAll())
-                .logout(logout -> logout.logoutUrl("/logout").permitAll());
-//                .csrf().disable()
-//                .httpBasic();
+//                    .formLogin(login -> login.loginPage("/login").permitAll())
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutUrl("/logout").permitAll())
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
+                .httpBasic();
 
         return http.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
