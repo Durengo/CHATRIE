b47dcf4e55b5c9c9f6b70c894629c6c6e8baa4e1
Sebastian <Sebastian.Termen@gmail.com>
2024-03-05 17:55:24 +0200

    Creating users with a builder - as it should be.

--- Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Helpers/DataInitializer.java
+++ Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Helpers/DataInitializer.java
@@ -1,19 +1,40 @@
 package lt.viko.eif.pi21e.Helpers;
 
+import lombok.AllArgsConstructor;
+import lt.viko.eif.pi21e.Authentication.AuthenticationResponse;
+import lt.viko.eif.pi21e.Entities.Role;
 import lt.viko.eif.pi21e.Entities.User;
 import lt.viko.eif.pi21e.Repositories.UserRepository;
+import lt.viko.eif.pi21e.Utility.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class DataInitializer implements CommandLineRunner {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) throws Exception {
-        userRepository.save(new User("somebody1", "123"));
-        userRepository.save(new User("somebody2", "123"));
+
+        User user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("123"))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        User user2 = User.builder()
+                .username("user2")
+                .password(passwordEncoder.encode("123"))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user2);
     }
 }
