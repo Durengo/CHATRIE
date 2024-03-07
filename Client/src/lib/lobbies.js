a522ac31ce28508b9bb3f0b08a1b6992d3e47e25
Sebastian <Sebastian.Termen@gmail.com>
2024-03-07 23:40:45 +0200

    Lobby and chat fetching.

--- Client/src/lib/lobbies.js
+++ Client/src/lib/lobbies.js
@@ -1,21 +1,44 @@
-import { apiBaseUrl, authToken, isLoggedIn, loginUsername } from '$lib/stores.js';
+import { writable } from 'svelte/store';
 
-export async function fetchChatRooms() {
-    const response = await fetch(`${apiBaseUrl}/lobbies`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify({ username, password }),
-        Authorization: `Bearer ${authToken}`
-    });
+export const chatHistory = writable([]);
 
-    if (response.ok) {
-        return await response.json();
-    } else {
-        throw new Error("Failed to fetch chatrooms");
-    }
- }
- 
- export async function fetchChatHistory(lobbyId) {
-     // Similar structure to fetchChatRooms, making a request 
-     // to an endpoint like '/api/chathistory?lobbyId=...'
- }
+export async function fetchChatHistory(lobbyId, authToken) {
+	const response = await fetch(`http://localhost:8090/chats/${lobbyId}`, {
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' },
+		Authorization: `Bearer ${authToken}`
+	});
+	const data = await response.json();
+
+	if (response.ok) {
+        console.log('Chat history:', data);
+		chatHistory.set(data);
+		return data;
+	} else {
+		throw new Error(`Error fetching chat history: ${data.message}`);
+	}
+}
+
+export async function fetchLobbies(username, authToken) {
+	// console.log(`Request: http://localhost:8090/lobbies/user/${username}}`);
+
+	const response = await fetch(`http://localhost:8090/lobbies/user/${username}`, {
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' },
+		// body: JSON.stringify({ username, password }),
+		Authorization: `Bearer ${authToken}`
+	});
+
+	if (response.ok) {
+		var data = await response.json();
+		console.log('Response: ', data);
+		return data;
+	} else {
+		throw new Error('Failed to fetch chatrooms');
+	}
+}
+
+// export async function fetchChatHistory(lobbyId) {
+// 	// Similar structure to fetchChatRooms, making a request
+// 	// to an endpoint like '/api/chathistory?lobbyId=...'
+// }
