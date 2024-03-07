00a21c17ee1f6b801771783bbe74cdbaf4ed2374
Sebastian <Sebastian.Termen@gmail.com>
2024-03-07 23:41:07 +0200

    Utility for formatting time.

---
+++ Client/src/lib/scripts/utils.js
@@ -0,0 +1,8 @@
+export function formatTimestamp(timestampString) {
+	const date = new Date(timestampString);
+	const hours = date.getHours().toString().padStart(2, '0');
+	const minutes = date.getMinutes().toString().padStart(2, '0');
+	const seconds = date.getSeconds().toString().padStart(2, '0');
+
+	return `${hours}:${minutes}:${seconds}`;
+}
