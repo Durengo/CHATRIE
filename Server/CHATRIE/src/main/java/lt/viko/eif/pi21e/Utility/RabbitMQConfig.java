dea1cece4ea1515a4bc4365f9410bea083664fd8
Sebastian <Sebastian.Termen@gmail.com>
2024-03-17 15:50:37 +0200

    RabbitMQ config class.

--- Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Utility/RabbitMQConfig.java
+++ Server/CHATRIE/src/main/java/lt/viko/eif/pi21e/Utility/RabbitMQConfig.java
@@ -1,74 +1,159 @@
 package lt.viko.eif.pi21e.Utility;
 
-import lt.viko.eif.pi21e.Entities.Lobby;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-/*
- * We define a fanout exchange (`lobby-exchange`) for broadcasting to multiple queues.
- * Queues are dynamically named based on the lobby ID which is a UUID (e.g., 'lobby-queue-aacf32e3-9195-4e36-b5de-6d7597013999').
- * Bindings connect the queues to the exchange.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String LOBBY_EXCHANGE_NAME = "lobby-exchange";
 
-    /**
-     * Creates a group of AMQP declarations (queues and bindings) for the lobby system.
-     * A separate queue is dynamically generated for each lobby based on its ID, and all queues are bound to the fanout exchange.
-     *
-     * @param lobbies A list of existing Lobby entities.
-     * @return A Declarables object containing the queue and binding definitions.
-     */
-    @Bean
-    public Declarables lobbyBindings(List<Lobby> lobbies) {
-        List<Declarable> bindings = lobbies.stream()
-                .map(lobby -> new Queue("lobby-queue-" + lobby.getLobbyId(), false))
-                .map(queue -> BindingBuilder.bind(queue).to(lobbyExchange()))
-                .collect(Collectors.toList());
-        bindings.add(lobbyExchange()); // Ensure the exchange itself is declared
-        return new Declarables(bindings);
+    @Value("${rabbitmq.queue.name}")
+    public String QUEUE_NAME;
+
+    @Value("${rabbitmq.exchange.name}")
+    public String EXCHANGE_NAME;
+
+    @Value("${rabbitmq.routing.key}")
+    public String ROUTING_KEY;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.http.port}")
+    private String port;
+    public final AmqpAdmin amqpAdmin;
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQConfig.class);
+
+    public RabbitMQConfig(AmqpAdmin amqpAdmin) {
+        this.amqpAdmin = amqpAdmin;
     }
 
-    /**
-     * Creates the fanout exchange used for broadcasting chat messages to lobby queues.
-     *
-     * @return A FanoutExchange instance with the configured exchange name.
-     */
     @Bean
-    FanoutExchange lobbyExchange() {
-        return new FanoutExchange(LOBBY_EXCHANGE_NAME);
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
     }
 
-    /**
-     * Provides the Jackson-based message converter for serializing/deserializing messages to/from JSON when using RabbitMQ.
-     *
-     * @return A Jackson2JsonMessageConverter instance.
-     */
+
     @Bean
-    public MessageConverter jsonMessageConverter() {
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter converter() {
         return new Jackson2JsonMessageConverter();
     }
 
-    /**
-     * Constructs the RabbitTemplate, a core Spring AMQP utility for sending and receiving messages. The configured message converter is applied.
-     *
-     * @param connectionFactory The connection factory for accessing the RabbitMQ broker.
-     * @return A fully configured RabbitTemplate instance.
-     */
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+
         return rabbitTemplate;
+    }
+
+    public Queue createQueue(UUID lobbyId, String sendToUsername) {
+        // TODO: Add logic so that we don't create or replace queues if we provide the same arguments.
+        String queueName = QUEUE_NAME + "-" + lobbyId + "-" + sendToUsername;
+        Queue queue = new Queue(queueName);
+
+        amqpAdmin.declareQueue(queue);
+
+        Binding binding = BindingBuilder.bind(queue)
+                .to(exchange())
+                .with(ROUTING_KEY);
+
+        amqpAdmin.declareBinding(binding);
+
+        return queue;
+    }
+
+    public void removeQueue(UUID lobbyId, String sendToUsername) {
+        String queueName = QUEUE_NAME + "-" + lobbyId + "-" + sendToUsername;
+        amqpAdmin.deleteQueue(queueName);
+    }
+
+    @Bean
+    public Queue autoDeleteUserQueue() {
+        return new AnonymousQueue();
+    }
+
+    @PostConstruct
+    private void purgeQueues() {
+        try {
+            String fullUrl = String.format("http://%s:%s/api/queues", host, port);
+            List<String> queues = getQueueNamesFromAPI(fullUrl);
+
+            if (queues.isEmpty())
+                logger.warn("no queues identified.");
+
+            for (var queue : queues) {
+                if (!queue.equals(QUEUE_NAME))
+                    purgeQueueDirect(queue);
+            }
+        } catch (Exception e) {
+            logger.error("Could not purge queues. (could be empty)");
+        }
+
+    }
+
+    public List<String> getQueueNamesFromAPI(String strURL) throws IOException {
+        String loginPassword = username + ":" + password;
+        String encoded = Base64.getEncoder().encodeToString(loginPassword.getBytes());
+
+        URL url = new URL(strURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "Basic " + encoded);
+
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+        }
+
+        List<String> queueNames = new ArrayList<>();
+        String[] split = response.toString().split("\"name\":\"");
+        for (int i = 1; i < split.length; i++) {
+            String nameRaw = split[i];
+            int index = nameRaw.indexOf("\"");
+            if (index > 0 && !nameRaw.startsWith("amq.")) {
+                String name = nameRaw.substring(0, index);
+                queueNames.add(name);
+            }
+        }
+        return queueNames;
+    }
+
+    private void purgeQueueDirect(String fullQueueName) {
+        amqpAdmin.deleteQueue(fullQueueName);
     }
 }
